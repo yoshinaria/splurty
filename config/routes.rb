b60991f0e114ce005a075867235489ba52fcb8fa
Akiko <yoshinariakiko@gmail.com>
2015-05-02 20:03:31 +0000

    added photos and style

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Splurty::Application.routes.draw do
   root 'quotes#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about' => 'quotes#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
